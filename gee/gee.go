b1b691765a9ecaee1b7fcfc04d3ff5195dd706f3
yuan <1209129548@qq.com>
2022-09-12 12:22:37 +0800

    fix: add router group

--- gee/gee.go
+++ gee/gee.go
@@ -7,28 +7,52 @@ import (
 // HandlerFunc defines the request handler used by gee
 type HandlerFunc func(ctx *Context)
 
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc //support middleware
+	parent      *RouterGroup  //support nesting
+	engine      *Engine       //all group share a engine instance
+}
+
 // Engine implement the interface of ServeHTTP
 type Engine struct {
-	route *router
+	*RouterGroup
+	router *router
+	groups []*RouterGroup //store all group
 }
 
 // New is the constructor of gee.Engine
 func New() *Engine {
-	return &Engine{route: newRoute()}
+	engine := &Engine{router: newRoute()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
-func (e *Engine) addRoute(method string, pattern string, handlerFunc HandlerFunc) {
-	e.route.addRoute(method, pattern, handlerFunc)
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		engine: engine,
+		parent: group,
+		prefix: group.prefix + prefix,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) addRoute(method string, comp string, handlerFunc HandlerFunc) {
+	pattern := group.prefix + comp
+	group.engine.router.addRoute(method, pattern, handlerFunc)
 }
 
 // GET defines the method to add GET request
-func (e *Engine) GET(pattern string, handlerFunc HandlerFunc) {
-	e.addRoute("GET", pattern, handlerFunc)
+func (group *RouterGroup) GET(pattern string, handlerFunc HandlerFunc) {
+	group.addRoute("GET", pattern, handlerFunc)
 }
 
 // POST defines the method to add POST request
-func (e *Engine) POST(pattern string, handlerFunc HandlerFunc) {
-	e.addRoute("POST", pattern, handlerFunc)
+func (group *RouterGroup) POST(pattern string, handlerFunc HandlerFunc) {
+	group.addRoute("POST", pattern, handlerFunc)
 }
 
 // Run defines the method to start a http server
@@ -38,5 +62,5 @@ func (e *Engine) Run(addr string) error {
 
 func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	ctx := newContext(req, w)
-	e.route.handler(ctx)
+	e.router.handler(ctx)
 }
--- main.go
+++ main.go
@@ -8,26 +8,35 @@ import (
 func main() {
 	r := gee.New()
 
-	r.GET("/", func(ctx *gee.Context) {
-		ctx.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
-	})
-	r.GET("/hello", func(ctx *gee.Context) {
-		ctx.String(http.StatusOK, "hello %s,you're at %s\n", ctx.Query("name"), ctx.Path)
-	})
-	r.POST("/login", func(ctx *gee.Context) {
-		ctx.JSON(http.StatusOK, gee.H{
-			"username": ctx.PostForm("username"),
-			"password": ctx.PostForm("password"),
-		})
-	})
-	r.GET("/hello/:name", func(c *gee.Context) {
-		// expect /hello/geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	r.GET("/index", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
 
-	r.GET("/assets/*filepath", func(c *gee.Context) {
-		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
-	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *gee.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		})
+
+		v1.GET("/hello", func(c *gee.Context) {
+			// expect /hello?name=geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *gee.Context) {
+			// expect /hello/geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *gee.Context) {
+			c.JSON(http.StatusOK, gee.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+
+	}
 
 	r.Run(":9999")
 }
