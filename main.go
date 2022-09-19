a970729969bf18f0eecb51de7fa3c3661f209a9c
yuan <1209129548@qq.com>
2022-09-19 11:10:58 +0800

    fix: add recovery

--- main.go
+++ main.go
@@ -1,51 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"github.com/yuan0408/gee"
-	"html/template"
 	"net/http"
-	"time"
 )
 
-type student struct {
-	Name string
-	Age  int8
-}
-
-func FormatAsDate(t time.Time) string {
-	year, month, day := t.Date()
-	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
-}
-
 func main() {
-	r := gee.New()
-	r.Use(gee.Logger())
-
-	r.SetFuncMap(template.FuncMap{
-		"FormatAsDate": FormatAsDate,
-	})
-	r.LoadHTMLGlob("templates/*")
-	r.Static("/assets", "./static")
-
-	stu1 := &student{Name: "Geektutu", Age: 20}
-	stu2 := &student{Name: "Jack", Age: 22}
-	r.GET("/", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "css.tmpl", nil)
-	})
-	r.GET("/students", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "arr.tmpl", gee.H{
-			"title":  "gee",
-			"stuArr": [2]*student{stu1, stu2},
-		})
+	engine := gee.Default()
+	engine.GET("/", func(ctx *gee.Context) {
+		ctx.String(http.StatusOK, "Hello yuan\n")
 	})
 
-	r.GET("/date", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "custom_func.tmpl", gee.H{
-			"title": "gee",
-			"now":   time.Date(2019, 8, 17, 0, 0, 0, 0, time.UTC),
-		})
+	engine.GET("/panic", func(ctx *gee.Context) {
+		names := []string{"yuan"}
+		ctx.String(http.StatusOK, names[100])
 	})
 
-	r.Run(":9999")
+	engine.Run(":9999")
 }
